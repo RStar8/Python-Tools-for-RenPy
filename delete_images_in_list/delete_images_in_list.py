ba2449ed538ca02792e47d2988ab9bc296d0779d
RStar8 <debtridip99@gmail.com>
2023-12-25 17:12:58 +0530

    Move to recycle

--- delete_images_in_list/delete_images_in_list.py
+++ delete_images_in_list/delete_images_in_list.py
@@ -1,6 +1,7 @@
 import os
+import send2trash
 
-def delete_files_in_directory(directory, file_names_to_delete):
+def delete_files_to_recycle_bin(directory, file_names_to_delete):
     deleted_count = 0
     for root, dirs, files in os.walk(directory):
         for file_name in files:
@@ -9,8 +10,8 @@ def delete_files_in_directory(directory, file_names_to_delete):
 
             if file_name_without_extension in file_names_to_delete:
                 file_path = os.path.join(root, file_name)
-                os.remove(file_path)
-                print(f"Deleted: {file_path}")
+                send2trash.send2trash(file_path)
+                print(f"Sent to recycle bin: {file_path}")
                 deleted_count += 1
 
     return deleted_count
@@ -24,8 +25,8 @@ try:
         print("First line:", first_line)
         image_file_names_to_delete = file.read().splitlines()
 
-    deleted_count = delete_files_in_directory(folder_to_search, image_file_names_to_delete)
-    print(f"Deletion complete. {deleted_count} files deleted.")
+    deleted_count = delete_files_to_recycle_bin(folder_to_search, image_file_names_to_delete)
+    print(f"Deletion complete. {deleted_count} files sent to recycle bin.")
 except FileNotFoundError:
     print(f"File not found: {list_file_path}")
 except Exception as e:
